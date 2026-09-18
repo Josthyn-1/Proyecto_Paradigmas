4ef41484ac1cd0036c1b991f168af829583addea
salaG301 <salaG301@poligran.edu.co>
2026-09-18 13:32:34 -0500

    Implement clinic turn management

---
+++ Proyecto/src/Taller/Nodo.java
@@ -0,0 +1,11 @@
+package Taller;
+
+public class Nodo {
+    Cliente cliente;
+    Nodo siguiente;
+
+    public Nodo(Cliente cliente){
+        this.cliente = cliente;
+        this.siguiente= null;
+    }
+}
