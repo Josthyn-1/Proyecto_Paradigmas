166fc3b9042332cf49bb948bbec9f51c43101702
Tu Nombre <tu-correo@ejemplo.com>
2026-09-21 14:48:41 -0500

    push con error

---
+++ Agenda/Agenda/src/main/java/co/edu/poligran/paradigmas/agenda/negocio/xd.java
@@ -0,0 +1,17 @@
+package co.edu.poligran.paradigmas.agenda.negocio;
+
+import co.edu.poligran.paradigmas.agenda.modelo.CorreoElectronico;
+import co.edu.poligran.paradigmas.agenda.modelo.Persona;
+import co.edu.poligran.paradigmas.agenda.modelo.Telefono;
+import co.edu.poligran.paradigmas.agenda.negocio.AgendaManager;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+
+public class xd {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+
+}
